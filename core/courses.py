8c83ee6123c664f4ec033e380c08042bc37a49b1
Will <wcpeters@uwm.edu>
2025-04-15 13:09:16 -0500

    Updated courses.py class skeleton

--- core/courses.py
+++ core/courses.py
@@ -3,76 +3,95 @@ class CourseManager:
     Manages course creation and instructor assignment.
     """
 
-    def __init__(self, course_id, course_code, title, semester, instructor_id):
-        self.course_id = course_id
-        self.title = title
-        self.semester = semester
-        self.instructor_id = instructor_id
+def __init__(self, course_id, course_code, title, semester, instructor_id):
+    self.title = title
+    self.course_id = course_id
+    self.semester = semester
+    self.instructor_id = instructor_id
 
-    def create_course(self, course_data):
-        """
-        Creates a new course.
+def __str__(self):
+    pass
 
-        Parameters:
-            course_data (dict): Dictionary of course fields
 
-        Preconditions: course_data must contain course fields, course does
-        not already exist.
+"""
+    Creates a new course.
+    Parameters: course_data (dict): Dictionary of course fields
+    Preconditions: course_data must contain course fields, course does
+    not already exist.
+    Postconditions: course is created successfully.
+"""
 
-        Postconditions: course is created successfully.
-        """
-        pass
+def create_course(self, course_data):
+    pass
 
-    def delete_course(self, course_id):
-        """
-        Parameters: course_id (int): The course ID of the course to be deleted.
 
-        Preconditions: course must exist.
+"""
+    Delete a course.
+    Parameters: course_id (int): The course ID of the course to be deleted.
+    Preconditions: course must exist.
+    Postconditions: the course is deleted successfully (everything set to none).
+"""
 
-        Postconditions: the course is deleted successfully (everything set to none).
-        """
-        pass
 
-        """
-        Parameters: new_title (String): The new course title.
-        
-        Preconditions: new_title is a String, does not conflict with existing course title.
-        
-        Postconditions: course is updated successfully.
-        """
+def delete_course(self, course_id):
+    pass
 
-    def update_id(self, new_id):
-        pass
 
-    """
-            Parameters: new_title (int): The new course ID.
+"""
+    Update a course's title.
+    Parameters: new_title (String): The new course title.
+    Preconditions: new_title is a String, does not conflict with existing course title.
+    Postconditions: course is updated successfully.
+"""
 
-            Preconditions: new_id is an integer, does not conflict with existing course ID.
 
-            Postconditions: course is updated successfully.
-            """
+def update_title(self, new_id):
+    pass
 
-    def update_title(self, new_title):
-        pass
 
-    """
-            Parameters: new_semester (String): The new course semester.
+"""
+    Return a particular course.
+    Parameters: course_id (int): The course ID.
+    Preconditions: course must exist.
+    Postconditions: none.
+"""
 
-            Preconditions: new_semester is a String, properly formatted as Fall/Spring, followed by a year.
 
-            Postconditions: course is updated successfully.
-            """
+def get_course(self, course_id):
+    pass
 
-    def update_semester(self, new_semester):
-        pass
 
-    """
-            Parameters: new_instructor (int): The new instructor's ID.
+"""
+    Update a course's id.
+    Parameters: new_id (int): The new course ID.
+    Preconditions: new_id is an integer, does not conflict with existing course ID.
+    Postconditions: course is updated successfully.
+"""
 
-            Preconditions: new_instructor is an integer, does not conflict with existing instructor.
 
-            Postconditions: course is updated successfully.
-            """
+def update_id(self, new_title):
+    pass
 
-    def update_instructor(self, new_instructor):
-        pass
+
+"""
+    Update the semester of a coures.
+    Parameters: new_semester (String): The new course semester.
+    Preconditions: new_semester is a String, properly formatted as Fall/Spring, followed by a year.
+    Postconditions: course is updated successfully.
+"""
+
+
+def update_semester(self, new_semester):
+    pass
+
+
+"""
+    Update a course's instrucor.
+    Parameters: new_instructor (int): The new instructor's ID.
+    Preconditions: new_instructor is an integer, does not conflict with existing instructor.
+    Postconditions: course is updated successfully.
+"""
+
+
+def update_instructor(self, new_instructor):
+    pass
---
+++ unittests/coursetest.py
@@ -0,0 +1,14 @@
+import unittest
+
+from core.courses import CourseManager
+
+class TestInit(unittest.TestCase):
+    pass
+
+class TestCreateCourse(unittest.TestCase):
+    pass
+
+class TestDeleteCourse(unittest.TestCase):
+    pass
+
+class
