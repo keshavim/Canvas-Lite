8ff5075a8ba0631f9bcc13120af6fd2cb253f415
Will <wcpeters@uwm.edu>
2025-04-15 13:33:14 -0500

    Updated courses.py class skeleton, coursetest.py

--- core/courses.py
+++ core/courses.py
@@ -1,97 +1,102 @@
-class CourseManager:
+class CourseManager(object):
     """
-    Manages course creation and instructor assignment.
+        Manages course creation and instructor assignment.
+        Parameters: course_id (int) - course id, title (String) - course title,
+        semester (String) - semester, the semester of the course, formatted Fall/Spring and a year,
+        instructor_id (int) - instructor id, the instructor of the course,
+        Preconditions: all input is properly formatted.
+        Postconditions: a course is created.
     """
 
-def __init__(self, course_id, course_code, title, semester, instructor_id):
-    self.title = title
-    self.course_id = course_id
-    self.semester = semester
-    self.instructor_id = instructor_id
+    def __init__(self, course_id, title, semester, instructor_id):
+        self.course_id = course_id
+        self.title = title
+        self.semester = semester
+        self.instructor_id = instructor_id
 
-def __str__(self):
-    pass
-
-
-"""
-    Creates a new course.
-    Parameters: course_data (dict): Dictionary of course fields
-    Preconditions: course_data must contain course fields, course does
-    not already exist.
-    Postconditions: course is created successfully.
-"""
-
-def create_course(self, course_data):
-    pass
+    def __str__(self):
+        pass
 
 
-"""
-    Delete a course.
-    Parameters: course_id (int): The course ID of the course to be deleted.
-    Preconditions: course must exist.
-    Postconditions: the course is deleted successfully (everything set to none).
-"""
+    """
+        Creates a new course.
+        Parameters: course_data (dict): Dictionary of course fields
+        Preconditions: course_data must contain course fields, course does
+        not already exist.
+        Postconditions: course is created successfully.
+    """
+
+    def create_course(self, course_data):
+        pass
 
 
-def delete_course(self, course_id):
-    pass
+    """
+        Delete a course.
+        Parameters: course_id (int): The course ID of the course to be deleted.
+        Preconditions: course must exist.
+        Postconditions: the course is deleted successfully (everything set to none).
+    """
 
 
-"""
-    Update a course's title.
-    Parameters: new_title (String): The new course title.
-    Preconditions: new_title is a String, does not conflict with existing course title.
-    Postconditions: course is updated successfully.
-"""
+    def delete_course(self, course_id):
+        pass
 
 
-def update_title(self, new_id):
-    pass
+    """
+        Update a course's title.
+        Parameters: new_title (String): The new course title.
+        Preconditions: new_title is a String, does not conflict with existing course title.
+        Postconditions: course is updated successfully.
+    """
 
 
-"""
-    Return a particular course.
-    Parameters: course_id (int): The course ID.
-    Preconditions: course must exist.
-    Postconditions: none.
-"""
+    def update_title(self, new_id):
+        pass
 
 
-def get_course(self, course_id):
-    pass
+    """
+        Return a particular course.
+        Parameters: course_id (int): The course ID.
+        Preconditions: course must exist.
+        Postconditions: none.
+    """
 
 
-"""
-    Update a course's id.
-    Parameters: new_id (int): The new course ID.
-    Preconditions: new_id is an integer, does not conflict with existing course ID.
-    Postconditions: course is updated successfully.
-"""
+    def get_course(self, course_id):
+        pass
 
 
-def update_id(self, new_title):
-    pass
+    """
+        Update a course's id.
+        Parameters: new_id (int): The new course ID.
+        Preconditions: new_id is an integer, does not conflict with existing course ID.
+        Postconditions: course is updated successfully.
+    """
 
 
-"""
-    Update the semester of a coures.
-    Parameters: new_semester (String): The new course semester.
-    Preconditions: new_semester is a String, properly formatted as Fall/Spring, followed by a year.
-    Postconditions: course is updated successfully.
-"""
+    def update_id(self, new_title):
+        pass
 
 
-def update_semester(self, new_semester):
-    pass
+    """
+        Update the semester of a coures.
+        Parameters: new_semester (String): The new course semester.
+        Preconditions: new_semester is a String, properly formatted as Fall/Spring, followed by a year.
+        Postconditions: course is updated successfully.
+    """
 
 
-"""
-    Update a course's instrucor.
-    Parameters: new_instructor (int): The new instructor's ID.
-    Preconditions: new_instructor is an integer, does not conflict with existing instructor.
-    Postconditions: course is updated successfully.
-"""
+    def update_semester(self, new_semester):
+        pass
 
 
-def update_instructor(self, new_instructor):
-    pass
+    """
+        Update a course's instrucor.
+        Parameters: new_instructor (int): The new instructor's ID.
+        Preconditions: new_instructor is an integer, does not conflict with existing instructor.
+        Postconditions: course is updated successfully.
+    """
+
+
+    def update_instructor(self, new_instructor):
+        pass
--- unittests/coursetest.py
+++ unittests/coursetest.py
@@ -3,6 +3,19 @@ import unittest
 from core.courses import CourseManager
 
 class TestInit(unittest.TestCase):
+    # test proper initialization
+    def test_init(self):
+        course = CourseManager(1, "title", "Spring 2024", 1)
+        self.assertEqual(course.course_id, 1)
+        self.assertEqual(course.title, "title")
+        self.assertEqual(course.semester, "Spring 2024")
+        self.assertEqual(course.instructor_id, 1)
+
+
+class TestString(unittest.TestCase):
+    pass
+
+class TestCourseManager(unittest.TestCase):
     pass
 
 class TestCreateCourse(unittest.TestCase):
@@ -11,4 +24,8 @@ class TestCreateCourse(unittest.TestCase):
 class TestDeleteCourse(unittest.TestCase):
     pass
 
-class
+class TestGetCourse(unittest.TestCase):
+    pass
+
+class TestUpdateCourse(unittest.TestCase):
+    pass
