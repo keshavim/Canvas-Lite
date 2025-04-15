0178248cc770f55974aec4f56a1aeae6a7a51b52
Hugo Gonzalez <hugoryangonzalez.hg@gmail.com>
2025-04-15 12:22:35 -0500

    Update login funciton to pass

--- core/login.py
+++ core/login.py
@@ -13,18 +13,16 @@ class AuthService:
         Authenticates and logs in a user.
         Returns True if login is successful, False otherwise.
         """
-        return TemplateView.as_view(template_name="dashboard.html")
-
         # user = authenticate(request, username=email, password=password)
         # if user is not None:
         #     login(request, user)
         #     return True
         # return False
+        pass
 
     def logout(self, request):
         """
         Logs out the current user.
         Always returns True.
         """
-        logout(request)
-        return True
+        pass
