9b64bd50ce8cdec18fb23371d36aedfabf820e49
Will <wcpeters@uwm.edu>
2025-04-29 09:20:20 -0500

    Updated non-admin views to redirect to login if a user attempts to access a page while not logged in

--- canvas_lite/webapp/views/pages.py
+++ canvas_lite/webapp/views/pages.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+
+# views for non-admin users
 
 def user_calendar(request):
-    return render(request, "standard_pages/calendar.html")
+    if request.user.is_authenticated:
+        return render(request, "standard_pages/calendar.html")
+    else:
+        return redirect("/login")
 
 def user_courses(request):
-    return render(request, "standard_pages/course_view.html")
+    if request.user.is_authenticated:
+        return render(request, "standard_pages/course_view.html")
+    else:
+        return redirect("/login")
 
 def user_profile(request):
-    return render(request, "standard_pages/user_profile.html")
+    if request.user.is_authenticated:
+        return render(request, "standard_pages/user_profile.html")
+    else:
+        return redirect("/login")
 
 def user_inbox(request):
-    return render(request, "standard_pages/inbox.html")
+    if request.user.is_authenticated:
+        return render(request, "standard_pages/inbox.html")
+    else:
+        return redirect("/login")
