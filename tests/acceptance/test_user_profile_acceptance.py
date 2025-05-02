49e936f4f5a2a24cfa327b551ef21738bdbd1cfc
Hugo Gonzalez <hugoryangonzalez.hg@gmail.com>
2025-05-01 22:41:09 -0500

    Create test_user_profile_acceptance.py

---
+++ tests/acceptance/test_user_profile_acceptance.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+class UserProfileAcceptanceTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="profileuser", password="testpass123")
+
+    def test_user_profile_requires_login(self):
+        response = self.client.get("/profile/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_profile_loads_for_logged_in_user(self):
+        self.client.login(username="profileuser", password="testpass123")
+        response = self.client.get("/profile/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "standard_pages/user_profile.html")
+        self.assertContains(response, "Hello, profileuser")
+        self.assertContains(response, "This is your profile view.")
+        self.assertContains(response, "Change Password")
