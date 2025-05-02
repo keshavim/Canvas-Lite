bf3dee80234b9f07b93e48c38dae2a4591615a09
Hugo Gonzalez <hugoryangonzalez.hg@gmail.com>
2025-05-01 22:40:06 -0500

    Create test_calendar_acceptance.py

---
+++ tests/acceptance/test_calendar_acceptance.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+class CalendarPageAcceptanceTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="calendaruser", password="testpass123")
+
+    def test_calendar_requires_login(self):
+        response = self.client.get("/calendar/")
+        self.assertEqual(response.status_code, 302)  # Redirect to login
+
+    def test_calendar_loads_for_logged_in_user(self):
+        self.client.login(username="calendaruser", password="testpass123")
+        response = self.client.get("/calendar/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "standard_pages/calendar.html")
+        self.assertContains(response, "Hello, calendaruser")
+        self.assertContains(response, "This is your calendar.")
