68ef169dd48d485cd40b21787a75b613994c6f24
Hugo Gonzalez <hugoryangonzalez.hg@gmail.com>
2025-05-01 22:40:51 -0500

    Create test_inbox_acceptance.py

---
+++ tests/acceptance/test_inbox_acceptance.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+class InboxAcceptanceTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="inboxuser", password="testpass123")
+
+    def test_inbox_requires_login(self):
+        response = self.client.get("/inbox/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_inbox_loads_for_logged_in_user(self):
+        self.client.login(username="inboxuser", password="testpass123")
+        response = self.client.get("/inbox/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "standard_pages/inbox.html")
+        self.assertContains(response, "Hello, inboxuser")
+        self.assertContains(response, "This is your inbox.")
