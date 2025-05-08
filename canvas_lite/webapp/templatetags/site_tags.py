c06d211b8dacbee79e40274b0cce550e6aa965d4
keshavim <keshavim2004@gmail.com>
2025-05-08 15:06:50 -0500

    some small formating updates

--- canvas_lite/webapp/templatetags/site_tags.py
+++ canvas_lite/webapp/templatetags/site_tags.py
@@ -1,15 +1,42 @@
+import json
+
 from django import template
+from django.forms import MultipleChoiceField
+
+from webapp.models.section import WEEKDAY_CHOICES, SEASON_CHOICES
 
 register = template.Library()
 
+# Helper to map value to label
+def get_label(value, choices):
+    return dict(choices).get(value, value)
+
 @register.filter
-def format_schedule(schedule):
-    return (
-        f"{', '.join(schedule.get('days', []))}, "
-        f"{schedule.get('start_time', 'N/A')}-{schedule.get('end_time', 'N/A')}, "
-        f"{schedule.get('semester', '')} {schedule.get('year', '')}"
-    )
+def format_schedule(schedule_json):
+    if not schedule_json:
+        return ""
+    try:
+        schedule = json.loads(schedule_json)
+    except (TypeError, ValueError):
+        return ""
+    # Map days to their labels
+    days = ', '.join(get_label(day, WEEKDAY_CHOICES) for day in schedule.get('days', []))
+    start = schedule.get('start_time', 'N/A')
+    end = schedule.get('end_time', 'N/A')
+    # Map semester to its label
+    semester = get_label(schedule.get('semester', ''), SEASON_CHOICES)
+    year = schedule.get('year', '')
+    return f"{days}, {start}-{end}, {semester} {year}"
+
 
 @register.filter
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+@register.filter
+def split(value, key):
+    return value.split(key)
+
+@register.filter
+def is_multiplechoice(field):
+    return isinstance(field.field, MultipleChoiceField)
